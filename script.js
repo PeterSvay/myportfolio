e0f4c2fc0db288879b06c07d9874038590efe792
Peter Svay <a1797794@student.adelaide.edu.au>
2023-03-06 17:49:42 +1030

    Added image

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-function button(){
-    console.log("Hello");
-}
+iam = ["Software Engineer.", "Web Designer.", "Artist."]
+var counter=0;
+
+setInterval(function button(){
+    counter++;
+    if (counter >= iam.length) {
+        counter = 0;
+    }
+    document.getElementById("Whatami").innerText = iam[counter];
+}, 5000)
+
