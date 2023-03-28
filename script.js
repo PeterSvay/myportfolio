1a5807df58682b2bafa11253aa9063b79c61e221
Peter Svay <a1797794@student.adelaide.edu.au>
2023-03-29 01:40:03 +1030

    cool slider implemented

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-iam = ["Software Engineer.", "Web Designer.", "Artist."]
-var counter=0;
-
-setInterval(function button(){
-    counter++;
-    if (counter >= iam.length) {
-        counter = 0;
-    }
-    document.getElementById("Whatami").innerText = iam[counter];
-}, 5000)
 
